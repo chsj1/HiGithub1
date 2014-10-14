d8811ab8afffe9f66a5e5583a1dd02f152e82e97
chsj2 <1019648568@qq.com>
2014-10-14 14:34:19 +0800

    submit total project

---
+++ src/HiGithub.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class HiGithub {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		int i;
+		for(i = 0 ; i < n ; ++i){
+			System.out.println("-------->hello");
+		}
+		
+	}
+}
